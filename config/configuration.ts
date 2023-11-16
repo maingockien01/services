ccaa43b185d444bb2c0549bcbdac6b684ecc5849
Kien Mai <kienmai@Kiens-Laptop.local>
2023-11-15 21:07:07 -0600

    Add config module

--- config/configuration.ts
+++ config/configuration.ts
@@ -4,9 +4,7 @@ import { join } from 'path';
 
 export default (folderPath: string = __dirname) => {
 	const files = readdirSync(folderPath);
-	const yamlFiles = files.filter(
-		(file) => file.endsWith('.yaml') || file.endsWith('.yml'),
-	);
+	const yamlFiles = files.filter((file) => file.endsWith('.yaml') || file.endsWith('.yml'));
 	const configs = yamlFiles.map((file) => {
 		const filePath = join(folderPath, file);
 		return yaml.load(readFileSync(filePath, 'utf8')) as Record<string, any>;
