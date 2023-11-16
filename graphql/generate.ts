8fba26c1ef565e60efe78efef98c718730450c2d
Kien Mai <kienmai@Kiens-Laptop.local>
2023-11-15 21:05:25 -0600

    Add graphql generate file

---
+++ graphql/generate.ts
@@ -0,0 +1,26 @@
+import { GraphQLDefinitionsFactory } from '@nestjs/graphql';
+import { readdirSync, unlinkSync } from 'fs';
+import { join } from 'path';
+
+const definitionsFactory = new GraphQLDefinitionsFactory();
+readdirSync(__dirname, { withFileTypes: true })
+	.filter((dirent) => dirent.isDirectory())
+	.map((dirent) => dirent.name)
+	.map((subdirectory) => {
+		const graphqlDirectoryPath = join(__dirname, subdirectory);
+		const commonDirectoryPath = join(__dirname, 'common');
+		const outputFilePath = join(process.cwd(), 'dtos', `${subdirectory}.ts`);
+
+		try {
+			unlinkSync(outputFilePath);
+		} catch (error) {
+			console.log(`File ${outputFilePath} does not exist`);
+		}
+
+		definitionsFactory.generate({
+			typePaths: [`${graphqlDirectoryPath}/**/*.graphql`, `${commonDirectoryPath}/**/*.graphql`],
+			path: outputFilePath,
+			outputAs: 'interface',
+			emitTypenameField: true,
+		});
+	});
